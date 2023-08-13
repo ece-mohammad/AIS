408fd57d15aa1b6bcf26b5307645846307cd2d31
Mohammad Mohsen <kuro.ece@gmail.com>
2023-08-13 13:27:07 +0300

    Changes: - Added secret key to `settings.py`

--- sia/settings.py
+++ sia/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY", default="django-insecure-z$#(&th-abnh7abtq1&jd9ywic4-dnjni9@xci=n22n5+ice4i")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
